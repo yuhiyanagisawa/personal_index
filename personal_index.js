c9c3626444bdd54f67613b9e421eb1e3480602e6
Yuhi Yanagisawa <yyanagisawa@me.com>
2016-01-17 10:44:34 -0500

    Second commit

--- personal_index.js
+++ personal_index.js
@@ -11,6 +11,7 @@ $( "#addTicker" ).click(function() {
   	$('#shares').append("<div>" + quantity + "</div>");
   }
 
+
 var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22' + ticker + '%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
 $.getJSON(url, function(data) {           
 var info = [];
@@ -24,8 +25,8 @@ $.each(data.query.results.quote, function(key, val)
         $('#price').append("<div id='" + ticker + "'>" + info[1] + "</div>");
         $('#change').append("<div id='" + ticker + "'>" + info[8] + "</div>");
         $('#value').append('<div>' + (info[1]*quantity).toLocaleString() + '</div>');
-                     
-    });
+        
+  });
 
 
 
